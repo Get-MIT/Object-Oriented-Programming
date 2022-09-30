a16243ff90b2ab05357eda32ffad909b83e7e2e2
Get-MIT <mail.getmit@gmail.com>
2022-09-30 22:46:23 +0300

    Added code of Lesson 3 - Video 3

---
+++ Lesson3-Video3/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student S1;
+		
+		S1 = new Student("George Mavroudis", "1001");
+		
+		Student S2 = new Student("Stelios Mavroudis");
+		
+		
+	}
+
+}
---
+++ Lesson3-Video3/Student.java
@@ -0,0 +1,19 @@
+
+public class Student {
+	
+	private String name;
+	private String id;
+	
+	// Constructor with 2 String parameters
+	public Student(String aName, String anID) {
+		name = aName;
+		id = anID;
+	}
+	
+	// Constructor with 1 String parameter
+	public Student(String aName) {
+		name = aName;
+		id = "9999";
+	}
+
+}
