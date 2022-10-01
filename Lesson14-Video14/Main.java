79cddd25eb633467a940643b465a0af689f8fae5
Get-MIT <mail.getmit@gmail.com>
2022-10-01 17:43:19 +0300

    Added code of Lesson 14 - Video 14 (until video time 5:00)
    It's an example of "static" and "final" in java.

---
+++ Lesson14-Video14/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+							// We can access PI Variable directly because it's public and STATIC. We can't change it because it's FINAL
+		System.out.println(Mathematics.PI); 
+
+	}
+
+}
---
+++ Lesson14-Video14/Mathematics.java
@@ -0,0 +1,6 @@
+
+public class Mathematics {
+	
+	public static final double PI = 3.14;
+
+}
