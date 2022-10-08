38da678811296045e02082a13c4d4af08fd15d7b
Get-MIT <mail.getmit@gmail.com>
2022-10-08 22:56:31 +0300

    Added code of Lesson 25 - Video 25 (at video time 13:00)
    HashSet can contain the same object only once.

--- Lesson25-Video25/src/Main.java
+++ Lesson25-Video25/src/Main.java
@@ -4,19 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-				// Doesn't extracts items in order
-				// Also HashSet can't have same object second time - could be used to ensure that all items are singular
-			HashSet<String> set = new HashSet<String>();
+			// Doesn't extracts items in order
+			// Also HashSet can't have same object second time - could be used to ensure that all items are singular
+		Collection<String> list = new ArrayList<>();
 		
-		set.add("John");
-		set.add("Nick");
-		set.add("Mary");
-		set.add("John");
+		list.add("John");
+		list.add("Nick");
+		list.add("Mary");
+		list.add("John");
 		
+		System.out.println("---ArrayList----");
+		for (String name: list) {
+			System.out.println(name);
+		}
+		
+			//Adding the ArrayList list to the HashSet set
+		Collection<String> set = new HashSet<>(list);  
+		
+		
+			// Doesn't allow same object twice
+		System.out.println("---TreeSet----");
 		for (String name: set) {
 			System.out.println(name);
 		}
-
 		
 		
 	}
