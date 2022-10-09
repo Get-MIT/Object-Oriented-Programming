4571cb16acf711e5d8d967a1060a12e07f3d56f7
Get-MIT <mail.getmit@gmail.com>
2022-10-09 17:16:14 +0300

    Added code of Lesson 27 - Video 27 (until video time 39:00)
    Playing around with Java Collections Methods

--- Lesson27-Video27/Main.java
+++ Lesson27-Video27/Main.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.*;
 
 
 /* Java Strongly Recommends that when you are adding your own types of elements in a HashSet you MUST Override the methods
@@ -13,23 +13,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-			// We know that these two accounts are the same account.
-			// But Java thinks they are different because they are different memory objects.
-		BankAccount BA1 = new BankAccount("001", 1500);
-		BankAccount BA2 = new BankAccount("001", 1500);
+		ArrayList<String> names = new ArrayList<String>();
 		
-		HashSet<BankAccount> set = new HashSet<BankAccount>();
+		names.add("Helen");
+		names.add("Bob");
+		names.add("Nick");
+		names.add("Mike");
+		names.add("Mary");
+				
+		System.out.println("---Sorted---");
+			// Sorts the specified list into ascending order, according to the natural ordering of its elements. 
+		Collections.sort(names); // All elements in the list must implement the Comparable interface.
+		System.out.println(names); // All collections override method "toString()" so i can call pass them in "println()" directly
 		
-		set.add(BA1);
-		set.add(BA2);
+		System.out.println("---Shuffle---");
+		Collections.shuffle(names); 
+		System.out.println(names);
+		System.out.println("---Shuffle-Again--");
+		Collections.shuffle(names); 
+		System.out.println(names);
 		
-		for (BankAccount account: set) {
-			System.out.println(account.getCode() + ", "
-								+ account.getBalance());
-		}
+		System.out.println("---Reverse---");
+		Collections.reverse(names); 
+		System.out.println(names);
 		
+		System.out.println("---Swap---");
+		Collections.swap(names, 2, 3); // Swaps 3rd with 4th (because counting starts with 0)
+		System.out.println(names);
 		
+		names.add("Helen");
+		names.add("Hellen");
 		
+		System.out.println("---Frequency---");	
+		int frequency = Collections.frequency(names, "Helen");
+		System.out.println(names);
+		System.out.println("Frequency of Helen is: " + frequency);
+		
+		System.out.println("---Max & Min---");
+		String max = Collections.max(names); // Returns the maximum (the last from a "short()" shorted state.
+		String min = Collections.min(names); // Returns the minimum (the first from a "short()" shorted state.
+		System.out.println("Max: " + max + " Min: " + min);
 	}
 
 }
